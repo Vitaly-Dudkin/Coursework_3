8c8289778971b810607fe0141d2e81856970d268
Vitaly <dudkin231@mail.ru>
2023-05-10 13:53:16 +0300

    added print()

--- src/main.py
+++ src/main.py
@@ -6,4 +6,5 @@ for operation in load_clients_operations(PATH)[:5]:
     print(get_date_operation(operation), get_description_operation(operation))
     print(get_from_operation(operation), get_to_operation(operation))
     print(get_amount(operation), get_currency(operation))
+    print()
 
