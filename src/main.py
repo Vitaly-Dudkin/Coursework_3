4e598ba4c3d1a1a2112ad42d019464d06511bd4c
Vitaly <dudkin231@mail.ru>
2023-05-12 10:06:13 +0300

    fix path

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 from utils import *
 
-PATH = 'clients_operations.json'
-
-for operation in load_clients_operations(PATH)[:5]:
+for operation in load_clients_operations()[:5]:
     print(get_date_operation(operation), get_description_operation(operation))
     print(get_from_operation(operation), get_to_operation(operation))
     print(get_amount(operation), get_currency(operation))
     print()
-
